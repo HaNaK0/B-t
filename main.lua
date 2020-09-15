bfa3a141af71033246b5a193c97006527894b0f4
HaNaK0 <saturn-v@hotmail.com>
2020-09-15 11:21:20 +0200

    set up som basic files

---
+++ main.lua
@@ -0,0 +1,13 @@
+function love.load()
+	local loadStart = love.timer.getTime()
+
+	print("Load Complete, took " .. love.timer.getTime() - loadStart .. " seconds")
+end
+
+function love.update(deltaTime)
+	
+end
+
+function love.draw()
+	
+end
