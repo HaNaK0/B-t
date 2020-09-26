b1d8deef30c50b8ef982974a8165dafb5270ac42
HaNaK0 <saturn-v@hotmail.com>
2020-09-26 15:06:59 +0200

    Added a component system

--- Game/GameObject.lua
+++ Game/GameObject.lua
@@ -1,18 +1,40 @@
 GameObject = {}
 
 function GameObject:new(xPos, yPos)
-	local newObject = {
+	local tempTransform = {
 		position = {
 			x = xPos, 
 			y = yPos
+		},
+
+		scale = {
+			x = 1,
+			y = 1
 		}
 	}
+
+	local newObject = {
+		transform = tempTransform
+	}
+
+
 	local newObjectMt = {}
 
-	newObjectMt.__index = GameObject
+	newObjectMt.__index = GameObject.Index
+	newObjectMt.__newindex = GameObject.AddComponent
 
 	setmetatable(newObject, newObjectMt)
 	return newObject
+end
+
+function GameObject:AddComponent(name, component)
+	assert(type(component) == "table", "trying to add " .. name .. " as a component but it's a " .. type(component) .. " components need to be tables")
+	self.components["name"] = component
+	component.parent = self
+end
+
+function GameObject:Index(name)
+	return GameObject[name] or self.components[name]
 end
 
 function GameObject:Draw()
@@ -20,15 +42,15 @@ function GameObject:Draw()
 		return
 	end
 	
-	if self.quad == nil then
-		love.graphics.draw(self.drawable, self.position.x, self.position.y)
-	else
-		love.graphics.draw(self.drawable, self.quad, self.position.x, self.position.y)
-	end
+	love.graphics.draw(self.drawable, self.transform.position.x, self.transform.position.y, self.transform.rotation, self.transform.scale.x, self.transform.scale.y)
 end
 
-function GameObject:Update(deltaTimer)
-	
+function GameObject:Update(deltaTime)
+	for name, component in pairs(self.components) do
+		if component.Update ~= nil then
+			component.Update(deltaTime)
+		end
+	end
 end
 
 local gameObjectMetaTable = {}
