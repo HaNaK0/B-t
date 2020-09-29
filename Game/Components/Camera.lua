53ebe68a04571b03b8ceb4a9b4f5648f4abfc09e
HaNaK0 <saturn-v@hotmail.com>
2020-09-29 18:21:18 +0200

    Updated Camera

--- Game/Components/Camera.lua
+++ Game/Components/Camera.lua
@@ -1,10 +1,14 @@
 Camera = {}
 
 function Camera:New(targetingScreen, width, height)
-	local newCamera = {}
+	local newCamera = {
+		transform = love.math.newTransform()
+	}
+	
 	if targetingScreen == false then
 		newCamera.canvas = love.graphics.newCanvas(width, height)
 	end
 
 	
 end
+
