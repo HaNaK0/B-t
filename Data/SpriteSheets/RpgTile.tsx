b50c06e0187369c9c240626ab10eaf7220f8f284
HaNaK0 <saturn-v@hotmail.com>
2020-10-24 12:04:43 +0200

    Going to add it properly later

--- Data/SpriteSheets/RpgTile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="RpgTile" tilewidth="64" tileheight="64" tilecount="1040" columns="40">
- <image source="../../Assets/SpriteSheets/RPGpack_sheet_2X.png" width="2560" height="1664"/>
-</tileset>
