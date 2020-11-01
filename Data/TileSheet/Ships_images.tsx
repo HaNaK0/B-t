9891d2ed01ab24da8bf3400d566cd7035598265c
HaNaK0 <saturn-v@hotmail.com>
2020-11-01 18:31:40 +0100

    Ship tile map

---
+++ Data/TileSheet/Ships_images.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Ships_images" tilewidth="66" tileheight="113" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="ship">
+  <image width="20" height="38" source="../../Assets/Ships/dinghyLarge1.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.30181" y="0.488177">
+    <polygon points="-0.650903,0 17.4116,0 17.7371,26.5243 10.5772,37.2642 6.83448,37.2642 -0.976354,21.6425 -0.813628,3.41724"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1" type="ship">
+  <image width="16" height="26" source="../../Assets/Ships/dinghySmall1.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.78998" y="0.488177">
+    <polygon points="0,0 13.5062,0 13.8317,8.94991 13.8317,14.3199 11.7162,20.178 7.32265,25.0598 4.88177,25.0598 -1.30181,17.0862 -1.13908,2.11543"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2" type="ship">
+  <image width="66" height="113" source="../../Assets/Ships/ship (1).png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="33" y="5.5">
+    <polygon points="0,0 10.25,2.25 18.25,13 19.25,18 19.25,61 17.75,80 7.5,100 0,107.5 -10.25,94.5 -18.25,74.75 -19.5,60.75 -18.75,17.25 -14.25,7 -8.5,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
